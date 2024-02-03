1a57811c630c18334aa05460bf909d14a963ad8c
Lanz <lanzmalto@su.edu.ph>
2024-02-04 00:53:14 +0800

    confirmatory commit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -34,10 +34,13 @@
 
         <div class="flex items-center justify-end mt-4">
             @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
+                <a class="underline text-sm text-gray-600 mr-5 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
                     {{ __('Forgot your password?') }}
                 </a>
             @endif
+            <a class="underline text-sm text-gray-600 mr-2 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ url('/register') }}">
+                {{ __('Not registered?') }}
+            </a>
 
             <x-primary-button class="ms-3">
                 {{ __('Log in') }}
